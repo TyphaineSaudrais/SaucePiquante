38d32a82567532cd74a8ebc8abbe3f8c818d7f17
Typhaine Prevost <saudraisprevosttyphaine@gmail.com>
2022-11-28 18:11:59 +0100

    first

---
+++ backend/routes/sauce.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+
+const sauceCtrl = require('../controllers/sauce');
+
+const multer = require('../middleware/multer-config');
+
+router.post('/', auth, multer,sauceCtrl.createSauce);
+router.get('/',  auth, sauceCtrl.getAllSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.put('/:id', auth,  multer,sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+
+
+module.exports = router;
